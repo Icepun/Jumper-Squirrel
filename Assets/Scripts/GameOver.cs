9e18446ffc8b1c1173693d6edb2e3830f8ecc3bf
BirSekViski <ayigits@gmail.com>
2023-08-14 22:44:38 +0300

    gameover menu added

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Bu metod, sahneyi tekrar yüklemek için kullanılabilir.
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void LevelSelectScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
